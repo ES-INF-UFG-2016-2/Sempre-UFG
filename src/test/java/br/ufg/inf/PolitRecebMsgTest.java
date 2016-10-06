fce8946d80e9b5f017277133d6a8cac186e28632
Weiner Silva <weinersilva@gmail.com>
2016-10-06 15:45:29 -0300

    Adiciona classe para teste do RF-PolitRecebMsg

---
+++ src/test/java/br/ufg/inf/PolitRecebMsgTest.java
@@ -0,0 +1,54 @@
+package br.ufg.inf;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert.*;
+
+@SuppressWarnings("unused")
+public class PolitRecebMsgTest {
+	
+	Usuario usuario;
+	PolitRecebMsg politica;
+	
+	@Before
+	public void setUp() throws Exception {
+		usuario = new Usuario();
+		politica = new PolitRecebMsg();
+	}
+
+	@Test
+	public void testUsuarioValido() {
+		assertEquals(true, usuario.UsuarioValido("teste@teste.com", "123"));
+	}
+	
+	@Test
+	public void testPolitMsgIndividual(){
+		assertEquals("RecebMsgIndividual",politica.selecionaPolitica(1));
+	}
+	
+	@Test
+	public void testPolitMsgAgregDia(){
+		assertEquals("RecebMsgAgregDia",politica.selecionaPolitica(2));
+	}
+	
+	@Test
+	public void testPolitMsgAgregSemana(){
+		assertEquals("RecebMsgAgregSemana",politica.selecionaPolitica(3));
+	}
+
+	@Test
+	public void testPolitMsgAgregMes(){
+		assertEquals("RecebMsgAgregMes",politica.selecionaPolitica(4));
+	}
+	
+	
+	@Test
+	public void testPolitNaoRecebMsg(){
+		assertEquals("NaoRecebMsg",politica.selecionaPolitica(5));
+	}
+	
+	
+}
