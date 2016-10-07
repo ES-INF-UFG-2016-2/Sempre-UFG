e01446e98e01bc17d5d6f5980b219eddb331c922
erivanbarbosa <erivan.nas@gmail.com>
2016-10-07 12:25:14 -0300

    Inclusão do arquivo POM e gitignore

--- src/main/java/br/ufg/inf/SalvaPlaniEgres.java
+++ src/main/java/br/ufg/inf/SalvaPlaniEgres.java
@@ -2,11 +2,9 @@ package br.ufg.inf;
 
 public class SalvaPlaniEgres implements SalvaPlaniEgresInterface 
 {
-
-	@Override
-	public void salvaPlaniEgres(Planilha planilha, String nomeDoArquivo, String caminhoDoArquivo) {
+	public void salvaPlaniEgres(Planilha planilha, String nomeDoArquivo, String caminhoDoArquivo)
+	{
 		// TODO Auto-generated method stub
-		
 	}
 	
 }
