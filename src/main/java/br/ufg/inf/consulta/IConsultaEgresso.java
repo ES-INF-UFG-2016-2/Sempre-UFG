d71efd8f58a7e89e5171f4e3d7de9c1eb7306d67
Gabriela Aimée <gabrielaimeeg@hotmail.com>
2016-10-10 22:02:04 -0300

    Métodos da interface para ConsultaEgresso otimizados.

--- src/main/java/br/ufg/inf/consulta/IConsultaEgresso.java
+++ src/main/java/br/ufg/inf/consulta/IConsultaEgresso.java
@@ -1,13 +1,11 @@
-package br.ufg.inf.RFExecCons;
+package main.java.br.ufg.inf.consulta;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 
-public interface IExecCons {
 
-    LinkedHashMap<String, String> executaConsultaDeEgressosPredefinida(int identificador);
+public interface IConsultaEgresso {
     LinkedHashMap<String, String> executaConsultaDeEgressosPredefinida(int identificador, List<String> parametros);
-    LinkedHashMap<String, String> executaConsultaDeEgressosAdHoc(List<String> colunasABuscar);
     LinkedHashMap<String, String> executaConsultaDeEgressosAdHoc(List<String> colunasABuscar, List<String> parametros);
     void atualizaDataUltimaConsulta(int identificadorConsulta);
 
