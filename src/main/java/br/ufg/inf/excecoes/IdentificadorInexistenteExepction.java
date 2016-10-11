a37bded0b01fbfd4245d6cf01e37a09688b76a37
Gabriela Aimée <gabrielaimeeg@hotmail.com>
2016-10-10 22:03:18 -0300

    Criada classe para exceção em casos de identificadores inexistentes nas consultas de egressos.

---
+++ src/main/java/br/ufg/inf/excecoes/IdentificadorInexistenteExepction.java
@@ -0,0 +1,8 @@
+package main.java.br.ufg.inf.excecoes;
+
+public class IdentificadorInexistenteExepction extends Exception {
+
+    public IdentificadorInexistenteExepction(String mensagem){
+        super(mensagem);
+    }
+}
