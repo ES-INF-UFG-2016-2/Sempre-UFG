fa07113193c07bc2b48cba0ca28957cc7713bd16
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-10-10 22:04:25 -0300

    Corrige compilação do branch G1

--- src/main/java/br/ufg/inf/bkp/Stub_Bkp.java
+++ src/main/java/br/ufg/inf/bkp/Stub_Bkp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.java.br.ufg.inf.bkp;
+package br.ufg.inf.bkp;
 
 import java.time.Period;
 
