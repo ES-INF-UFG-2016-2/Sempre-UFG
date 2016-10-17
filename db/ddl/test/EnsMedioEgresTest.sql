963fe2050eb4ee3f973e63761bc5717893a99094
RenanOfugi <renangyn2010@hotmail.com>
2016-10-17 17:39:31 -0300

    Script SQL para teste: Finalizado

--- db/ddl/test/EnsMedioEgresTest.sql
+++ db/ddl/test/EnsMedioEgresTest.sql
@@ -1,3 +1,10 @@
+/*
+ * Não foram realizadas verificações nos relacionamentos exceto o relacionamento
+ * da tabela EnsMedioEgres com a tabela LocalizGeograf por haver a restrição de
+ * que apenas um elemento da tabela EnsMedioEgres relaciona somente com uma ou nenhuma
+ * tabela de LocalizGeograf.
+ */
+
 -- Criação de teste para verificar a inserção dos dados somente na tabela Egresso
 
 delimiter //
@@ -26,6 +33,8 @@ begin
 end
 //
 
+delimiter ;
+
 -- Criação de teste para verificar a inserção dos dados somente na tabela Instituição de Ensino Medio
 
 delimiter //
@@ -49,6 +58,7 @@ begin
 end
 //
 
+delimiter ;
 -- Criação de teste para verificar a inserção dos dados somente na tabela de Localização geográfica
 
 delimiter //
@@ -76,6 +86,36 @@ begin
 end
 //
 
+delimiter ;
+
+-- Criação de teste para verificar a consistencia dos dados na tabela HistEnsMedio
+
+delimiter //
+
+create procedure inserir_historicoEnsinoMedio(
+  in id_egresso bigint,
+  in id_EnsMedio text,
+  in mes_inicio integer,
+  in ano_inicio integer,
+  in mes_fim integer,
+  in ano_fim integer
+)
+begin
+
+  declare exit handler for 1265
+  select 'Tipo do dado nao permitido';
+
+  declare exit handler for 1318
+  select 'chamada necessita parametros';
+
+  insert into HistEnsMedio (id_egresso, id_EnsMedio, mes_inicio, ano_inicio, mes_fim, ano_fim)
+    values (id_egresso, id_EnsMedio, mes_inicio, ano_inicio, mes_fim, ano_fim);
+
+end
+//
+
+delimiter ;
+
 -- Criação de teste para verificar os relacionamentos
 
 delimiter //
@@ -86,7 +126,7 @@ create function verificaQuantidade(i int)
   begin
     declare s text;
 
-    if i > 1 then s = 'erro relacionamento um para muitos';
+    if i > 1 then s = 'erro de relacionamento: Possivel quebra N:1';
     else s = 'relacionamento correto';
     end if
 
@@ -97,12 +137,72 @@ delimiter ;
 
 delimiter //
 
-create procedure buscaRows(out x int)
+create procedure buscaRows(out quantidade int)
   begin
-    select count(*) into x from EnsMedioEgres
-    inner join LocalizGeograf on EnsMedioEgres.id_localizacao = LocalizGeograf.id_localizacao;
-    order by EnsMedioEgres.nome_iem asc;
+
+  select id_localizacao, count(*) into quantidade from EnsMedioEgres group by id_localizacao;
 
   end //
 
 delimiter ;
+
+-- Execução dos testes de stored procedures para Egresso.
+
+call inserir_somenteEgresso('Joao Aluísio', 'Marilia Pereira de Sousa', '20/03/1995', NULL, NULL, 2, 1, 1);
+
+call inserir_somenteEgresso('Diego Pereira da Silva', 'Tania Maria ALlves', '12/12/1995', NULL, NUll, 70, 1, 1);
+
+call inserir_somenteEgresso('Diego Pereira da Silva', 3.7, '12/12/1995', NULL, NUll, 2, 1, 1);
+
+call inserir_somenteEgresso('Joao Aluísio', 'Marilia Pereira de Sousa', 20/03/1995, NULL, NULL, 2, 1, 1);
+
+call inserir_somenteEgresso('Joao Aluísio', 'Marilia Pereira de Sousa', '20/03/1995', 'teste com string', NULL, 2, 1, 1);
+
+call inserir_somenteEgresso('Joao Aluísio', 'Marilia Pereira de Sousa', '20/03/1995', NULL, 2, 2, 1, 1);
+
+-- Execução dos testes de stored procedures para LocalizGeograf
+
+call inserir_localizacaoGeografica('Goiania', 'Goias', 'Brasil', 'GO', -16.6868910, -49.2647940, 1);
+
+call inserir_localizacaoGeografica('Goiania', 'Goias', 'Brasil', 'GO', NULL, NULL, 70);
+
+call inserir_localizacaoGeografica('Goiania', 'Goias', 'Brasil', 'GO', -16.6868910, -49.2647940, 'teste');
+
+call inserir_localizacaoGeografica('Goiania', 'Goias', 'Brasil', 2, -16.6868910, -49.2647940, 1);
+
+call inserir_localizacaoGeografica('Goiania', 'Goias', 2, 'GO', -16.6868910, -49.2647940, 1);
+
+call inserir_localizacaoGeografica(2, 'Goias', 'Brasil', 'GO', -16.6868910, -49.2647940, 1);
+
+call inserir_localizacaoGeografica('Goiania', 2, 'Brasil', 'GO', -16.6868910, -49.2647940, 1);
+
+call inserir_localizacaoGeografica('Goiania', 'Goias', 'Brasil', 'GO', -16.6868910, 'teste para verificação', 1);
+
+call inserir_localizacaoGeografica('Goiania', 'Goias', 'Brasil', 'GO', 'teste para verificação', -49.2647940, 1);
+
+-- Execução de testes de stored procedure para EnsMedioEgres
+
+call inserir_ensinoMedio(1, 2, 'Colegio Estadual Fulano de tal', 2);
+
+call inserir_ensinoMedio(1, 2, 2, 2);
+
+call inserir_ensinoMedio(11, 2300, 'Colegio Estadual Fulano de tal', 5);
+
+call inserir_ensinoMedio('teste de inserção invalida', 2, 'Colegio Estadual Fulano de tal', 2);
+
+call inserir_ensinoMedio(1, 'teste de inserção invalida', 'Colegio Avila', 4);
+
+call inserir_historicoEnsinoMedio(1, 'Colegio Vera Cruz II', 03, 10, 04, 13);
+
+call inserir_historicoEnsinoMedio('teste de inserção invalida', 'Colegio Vera Cruz II', 03, 10, 04, 13);
+
+call inserir_historicoEnsinoMedio(1, 3, 03, 10, 04, 13);
+
+call inserir_historicoEnsinoMedio(1, 'Colegio Vera Cruz II', 8288, 99999, 99999, 8873);
+
+call inserir_historicoEnsinoMedio(1, 'Colegio Vera Cruz II', 'teste de inserção invalida', 10, 04, 13);
+
+-- Execução do teste de relacionamento
+
+call buscaRows(@quantidadeRows);
+select verificaQuantidade(select @quantidadeRows);
