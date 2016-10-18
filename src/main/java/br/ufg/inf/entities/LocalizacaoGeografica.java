ef4c0db86d1a2cfa37e0583509a098d1ba069fb9
sadboyz <lucaslupus1@gmail.com>
2016-10-17 22:59:00 -0200

    Testes CursosOutrasIesEgres

---
+++ src/main/java/br/ufg/inf/entities/LocalizacaoGeografica.java
@@ -0,0 +1,56 @@
+package entities;
+
+public class LocalizacaoGeografica {
+	
+	private String nomeDaCidade;
+	
+	private String nomeDaUnidadeFederativa;
+	
+	private String nomeDoPais;
+	
+	private String siglaDaUnidadeFederativa;
+	
+	private Float latitude;
+	
+	private Float longitude;
+	
+	public String getNomeDaCidade() {
+		return nomeDaCidade;
+	}
+	public void setNomeDaCidade(String nomeDaCidade) {
+		this.nomeDaCidade = nomeDaCidade;
+	}
+	public String getNomeDaUnidadeFederativa() {
+		return nomeDaUnidadeFederativa;
+	}
+	public void setNomeDaUnidadeFederativa(String nomeDaUnidadeFederativa) {
+		this.nomeDaUnidadeFederativa = nomeDaUnidadeFederativa;
+	}
+	public String getNomeDoPais() {
+		return nomeDoPais;
+	}
+	public void setNomeDoPais(String nomeDoPais) {
+		this.nomeDoPais = nomeDoPais;
+	}
+	public String getSiglaDaUnidadeFederativa() {
+		return siglaDaUnidadeFederativa;
+	}
+	public void setSiglaDaUnidadeFederativa(String siglaDaUnidadeFederativa) {
+		this.siglaDaUnidadeFederativa = siglaDaUnidadeFederativa;
+	}
+	public Float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Float latitude) {
+		this.latitude = latitude;
+	}
+	public Float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Float longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+	
+}
---
+++ src/main/java/br/ufg/inf/entities/Nivel.java
@@ -0,0 +1,6 @@
+package entities;
+
+public enum Nivel {
+
+	Bacharelado,Licenciatura,Aperfeiçoamento,Especialização,Mestrada,Doutorado;
+}
