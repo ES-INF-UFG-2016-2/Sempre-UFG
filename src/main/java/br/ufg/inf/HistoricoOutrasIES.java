5ce6ff56511210b1459fe2a032fd5cf0451e7b1b
sadboyz <lucaslupus1@gmail.com>
2016-10-19 23:30:59 -0200

    Testes-RD-CursOutrasIesEgres

---
+++ src/main/java/br/ufg/inf/HistoricoOutrasIES.java
@@ -0,0 +1,40 @@
+package br.ufg.inf.modelo;
+
+public class HistoricoOutrasIES {
+
+	
+	private Integer mesInicio;
+	
+	private Integer mesFim;
+	
+	private Integer anoInicio;
+	
+	private Integer anoFim;
+	
+	public Integer getMesInicio() {
+		return mesInicio;
+	}
+	public void setMesInicio(Integer mesInicio) {
+		this.mesInicio = mesInicio;
+	}
+	public Integer getMesFim() {
+		return mesFim;
+	}
+	public void setMesFim(Integer mesFim) {
+		this.mesFim = mesFim;
+	}
+	public Integer getAnoInicio() {
+		return anoInicio;
+	}
+	public void setAnoInicio(Integer anoInicio) {
+		this.anoInicio = anoInicio;
+	}
+	public Integer getAnoFim() {
+		return anoFim;
+	}
+	public void setAnoFim(Integer anoFim) {
+		this.anoFim = anoFim;
+	}
+	
+	
+}
---
+++ src/main/java/br/ufg/inf/TipoInstituicao.java
@@ -0,0 +1,6 @@
+package br.ufg.inf.modelo;
+
+public enum TipoInstituicao {
+
+	Federal,Estadual,Municipal,Particular;
+}
