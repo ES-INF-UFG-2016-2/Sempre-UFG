b931d10b0c5ce25769dc2b66fa6252aa47a81def
Gleibson Wemerson Silva Borges <gleibsongyn@gmail.com>
2016-10-20 17:01:00 -0200

    Adiciona stub de AtuProfisEgresTest

---
+++ src/main/java/br/ufg/inf/AtuProfisEgres.java
@@ -0,0 +1,31 @@
+public class AtuProfisEgres {
+
+
+    public Boolean inserirDataInicio(int data) {
+        return true;
+    }
+
+    public boolean inserirDataFim(int data) {
+        return true;
+    }
+
+    public boolean inserirFormaIngresso(String forma) {
+        return true;
+    }
+
+    public boolean inserirRendaMensaMedia(float renda) {
+        return true;
+    }
+
+    public boolean inserirSatisfacaoComRenda(int satisfacao) {
+        return true;
+    }
+
+    public boolean inserirPerspectivaFuturo(int perspectiva) {
+        return true;
+    }
+
+    public boolean inserirComentario(String comentario) {
+        return true;
+    }
+}
