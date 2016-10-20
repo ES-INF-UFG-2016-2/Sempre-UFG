8877fec94d1fd3e178dd3691d2cff1ff8318287b
Gleibson Wemerson Silva Borges <gleibsongyn@gmail.com>
2016-10-20 16:51:51 -0200

    Update AtuProfisEgresTest.java

--- src/test/java/br/ufg/inf/AtuProfisEgresTest.java
+++ src/test/java/br/ufg/inf/AtuProfisEgresTest.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class TestAtuProfisEgres {
+public class AtuProfisEgresTest {
 
     @Test
     public void testInserirDataInicioCorreta() {
