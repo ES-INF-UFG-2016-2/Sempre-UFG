fbf710514c002d186f9235ac000a61667c20a7f5
pauloXtr3m <paulo-gyn@outlook.com>
2016-10-21 12:03:04 -0200

    Adicionado código de login(apenas para conclusão do teste)

---
+++ src/main/java/br/ufg/inf/servlet/LoginServlet.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.servlet;
+
+/**
+ *
+ * @author paulo
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+public class LoginServlet extends HttpServlet{
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+       
+        if(Validate.checkUser(email, pass))
+        {
+            out.print("Welcome");  
+            HttpSession session=request.getSession();
+            session.setAttribute(email, email); 
+        }
+        else
+        {
+           out.print("Sorry, username or password error!");  
+           request.getRequestDispatcher("login.html").include(request, response);  
+        }
+        out.close();  
+    }  
+}
---
+++ src/main/java/br/ufg/inf/servlet/Validate.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.servlet;
+
+/**
+ *
+ * @author paulo
+ */
+import java.sql.*;
+
+public class Validate
+ {
+     public static boolean checkUser(String email,String pass) {
+         boolean st = false;
+     
+         return st;                 
+     }   
+
+  
+}
