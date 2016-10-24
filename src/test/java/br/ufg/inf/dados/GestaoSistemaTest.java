654580bf785369d1435e16738e387e8d121e526c
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-10-24 16:39:34 -0200

    Adiciona testes para verificar se tabela sempre ufg é singleton e verificar relacionamento com a tabela usuário.

--- src/test/java/br/ufg/inf/dados/GestaoSistemaTest.java
+++ src/test/java/br/ufg/inf/dados/GestaoSistemaTest.java
@@ -27,7 +27,10 @@ public class GestaoSistemaTest {
     @After
     public void limpaTabelas() {
         try {
-            executaSqlComStatement("TRUNCATE TABLE SempreUFG");
+            executaSqlComStatement("SET FOREIGN_KEY_CHECKS=0;");
+            executaSqlComStatement("TRUNCATE TABLE SEMPREUFG;");
+            executaSqlComStatement("TRUNCATE TABLE USUARIO;");
+            executaSqlComStatement("SET FOREIGN_KEY_CHECKS=1;");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -41,28 +44,28 @@ public class GestaoSistemaTest {
     @Given("testaConexaoComBancoDeDados")
     @Test
     public void testaSeTabelaSempreUFGExiste() {
-        boolean tabelaExiste = verificaSeTabelaExiste("SempreUFG");
+        boolean tabelaExiste = verificaSeTabelaExiste("SEMPREUFG");
         Assert.assertTrue(tabelaExiste);
     }
 
     @Given("testaConexaoComBancoDeDados")
     @Test
     public void testaSeTabelaParametroExiste() {
-        boolean tabelaExiste = verificaSeTabelaExiste("Parametro");
+        boolean tabelaExiste = verificaSeTabelaExiste("PARAMETRO");
         Assert.assertTrue(tabelaExiste);
     }
 
     @Given("testaConexaoComBancoDeDados")
     @Test
     public void testaSeTabelaBackupExiste() {
-        boolean tabelaExiste = verificaSeTabelaExiste("Backup");
+        boolean tabelaExiste = verificaSeTabelaExiste("BACKUP");
         Assert.assertTrue(tabelaExiste);
     }
 
     @Given("testaConexaoComBancoDeDados")
     @Test
     public void testaSeTabelaRestauracaoExiste() {
-        boolean tabelaExiste = verificaSeTabelaExiste("Restauracao");
+        boolean tabelaExiste = verificaSeTabelaExiste("RESTAURACAO");
         Assert.assertTrue(tabelaExiste);
     }
 
@@ -82,7 +85,7 @@ public class GestaoSistemaTest {
 
     @Test
     public void testaInsercaoNaTabelaSempreUfgComUsuarioInexistenteLancaExcecao() {
-        String queryInserirSempreUFG = "INSERT INTO SempreUFG (nome_sistema, timestamp_isstalacao, id_Usuario) " +
+        String queryInserirSempreUFG = "INSERT INTO SEMPREUFG (nome_sistema, timestamp_isstalacao, id_Usuario) " +
             "VALUES('Sempre UFG', now(), 123);";
 
         boolean lancouExcecao = false;
@@ -92,6 +95,43 @@ public class GestaoSistemaTest {
             lancouExcecao = true;
             e.printStackTrace();
         }
+        Assert.assertTrue(lancouExcecao);
+    }
+
+    @Given("testaSeTabelaSempreUFGExiste")
+    @Test
+    public void testaInserirNaTabelaSempreUfgComUsuarioNaoExistenteDeveLancarExcecao() {
+        boolean lancouExcecao = false;
+        try {
+            inserirTabelaSempreUFG("Sempre UFG", 12345);
+        } catch (SQLException e) {
+            lancouExcecao = true;
+            e.printStackTrace();
+        }
+        Assert.assertTrue(lancouExcecao);
+    }
+
+    @Given("testaSeTabelaSempreUFGExiste")
+    @Test
+    public void testaInserirDoisRegistrosNaTabelaSempreUFGLancaExcecao() {
+        int idUsuario = 1234567;
+        boolean usuarioCadastradoComSucesso;
+        try {
+            inserirUsuario(idUsuario, "teste1@teste.com", "1234", "Usuário Teste 1", 123456789);
+            usuarioCadastradoComSucesso = true;
+        } catch (SQLException e) {
+            usuarioCadastradoComSucesso = false;
+        }
+
+        boolean lancouExcecao = false;
+        try {
+            inserirTabelaSempreUFG("SempreUFG", idUsuario);
+            inserirTabelaSempreUFG("Segundo Sistema", idUsuario);
+        } catch (SQLException e) {
+            lancouExcecao = true;
+            e.printStackTrace();
+        }
+        Assert.assertTrue("Erro ao cadastrar usuário", usuarioCadastradoComSucesso);
         Assert.assertTrue(lancouExcecao);
     }
 
@@ -110,6 +150,31 @@ public class GestaoSistemaTest {
             e.printStackTrace();
         }
         return tabelaExiste;
+    }
+
+    private static void inserirTabelaSempreUFG(String nomeSistema, int idUsuario) throws SQLException {
+        String queryInserirSempreUFG = "INSERT INTO SEMPREUFG (nome_sistema, timestamp_isstalacao, id_Usuario) " +
+            "VALUES(?,?,?);";
+        PreparedStatement preparedStatement = conexaoBD.prepareStatement(queryInserirSempreUFG);
+        preparedStatement.setString(1, nomeSistema);
+        preparedStatement.setDate(2, new Date(1));
+        preparedStatement.setInt(3, idUsuario);
+        preparedStatement.execute();
+    }
+
+    private static void inserirUsuario(int idUsuario, String email, String senha, String nomeSocial, int CPF) throws SQLException {
+        String inserirUsuarioSQL = "insert into USUARIO (idUsuario, email_principal,  senha,  nome_social,  CPF," +
+            "  recebe_divulgacao, timestamp_cadastramento) VALUES (?,?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = conexaoBD.prepareStatement(inserirUsuarioSQL);
+        preparedStatement.setInt(1, idUsuario);
+        preparedStatement.setString(2, email);
+        preparedStatement.setString(3, senha);
+        preparedStatement.setString(4, nomeSocial);
+        preparedStatement.setInt(5, CPF);
+        preparedStatement.setString(6, "mensal");
+        preparedStatement.setDate(7, new Date(1));
+        preparedStatement.execute();
+
     }
 
     private static void executaSqlComStatement(String sql) throws SQLException {
