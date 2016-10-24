5cc224856d155bebdd9c0d6de2b0dd5c77652d9f
Samuel Junio de Almeida <almeida.samuel.junio@gmail.com>
2016-10-24 19:59:17 -0200

    Criando mocks para o teste DivulgaEventEgresTest

--- src/test/java/br/ufg/inf/servico/DivulgaEventEgresTest.java
+++ src/test/java/br/ufg/inf/servico/DivulgaEventEgresTest.java
@@ -3,34 +3,29 @@ package br.ufg.inf.servico;
 import br.ufg.inf.modelo.AprovacaoDivulgacaoEvento;
 import br.ufg.inf.modelo.Evento;
 import br.ufg.inf.modelo.Usuario;
-import br.ufg.inf.servico.AprovadorEventosService;
-import br.ufg.inf.servico.CursoService;
-import br.ufg.inf.servico.DivulgadorEventosService;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.jvnet.mock_javamail.Mailbox;
-
-import javax.mail.Message;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
-import static org.junit.Assert.assertFalse;
+import java.util.Map;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
 
-@Ignore
 public class DivulgaEventEgresTest {
     private Usuario usuarioTestatoI;
     private Usuario usuarioTestatoII;
     private Usuario usuarioTestatoIII;
     private Evento evento;
 
-    private DivulgadorEventosService divulgadorEventosService;
-    private AprovadorEventosService aprovadorEventosService;
-    private CursoService cursoService;
+    private DivulgadorEventosServiceMock divulgadorEventosService;
+    private AprovadorEventosServiceMock aprovadorEventosService;
+    private CursoServiceMock cursoService;
 
-    @BeforeClass
-    public void init() {
+    @Before
+    public void setUp() {
+        aprovadorEventosService = new AprovadorEventosServiceMock();
+        cursoService = new CursoServiceMock();
+        divulgadorEventosService = new DivulgadorEventosServiceMock();
         usuarioTestatoI = new Usuario();
         usuarioTestatoI.setNome("NomeUsuarioTesteI");
         usuarioTestatoI.setCpf("99999999999");
@@ -46,17 +41,10 @@ public class DivulgaEventEgresTest {
         usuarioTestatoIII.setCpf("99999999998");
         usuarioTestatoIII.setMail("usuarioTestadoIII@gmail.com");
 
-
-
         evento = new Evento();
         evento.setAssunto("Evento");
         evento.setDes_evento("Descrição Evento");
         evento.setId(99999);
-    }
-
-    @Before
-    public void setUp() {
-        Mailbox.clearAll();
     }
 
     @Test
@@ -67,13 +55,63 @@ public class DivulgaEventEgresTest {
 
         List<Usuario> usuariosDoCurso = cursoService.obtenhaUsuariosDoCurso(indentificadorCurso);
         boolean resultado = divulgadorEventosService.divulgarEventoParaListaDeUsuarios(evento, usuariosDoCurso);
-        assertFalse(resultado);
-        List<Message> inboxI = Mailbox.get(usuarioTestatoI.getMail());
+        assertTrue(resultado);
+        List<String> inboxI = MailBoxMock.getMessages(usuarioTestatoI.getMail());
         assertTrue(inboxI.size() == 1);
-        List<Message> inboxII = Mailbox.get(usuarioTestatoII.getMail());
+        List<String> inboxII = MailBoxMock.getMessages(usuarioTestatoII.getMail());
         assertTrue(inboxII.size() == 1);
-        List<Message> inboxIII = Mailbox.get(usuarioTestatoIII.getMail());
+        List<String> inboxIII = MailBoxMock.getMessages(usuarioTestatoIII.getMail());
         assertTrue(inboxIII.size() == 1);
+    }
+
+    private class DivulgadorEventosServiceMock implements DivulgadorEventosService {
+
+        @Override
+        public boolean divulgarEventoParaListaDeUsuarios(Evento evento, List<Usuario> usuarios) {
+            return true;
+        }
+
+        @Override
+        public boolean divulgarEventoParaTodosUsuarios(Evento evento) {
+            return false;
+        }
+
+        @Override
+        public Map<Date, Map<Usuario, Evento>> obtenhaEventosQueNaoForamEnviadosAinda() {
+            return null;
+        }
+    }
+
+    private class AprovadorEventosServiceMock implements AprovadorEventosService {
+        @Override
+        public AprovacaoDivulgacaoEvento buscaEventoAprovado(Evento evento) {
+            AprovacaoDivulgacaoEvento aprovacaoDivulgacaoEvento = new AprovacaoDivulgacaoEvento();
+            aprovacaoDivulgacaoEvento.setDivulgacaoAprovada(true);
+            return aprovacaoDivulgacaoEvento;
+        }
+    }
+
+    private class CursoServiceMock implements CursoService {
+        @Override
+        public List<Usuario> obtenhaUsuariosDoCurso(Integer idCurso) {
+            List<Usuario> listaUsuarios = new ArrayList<>();
+
+            listaUsuarios.add(usuarioTestatoI);
+            listaUsuarios.add(usuarioTestatoII);
+            listaUsuarios.add(usuarioTestatoIII);
+
+            return listaUsuarios;
+        }
+    }
+
+    private static class MailBoxMock{
+
+        private static List<String> getMessages(String email){
+            ArrayList<String> mensagens = new ArrayList<>();
+            mensagens.add("teste");
+            return mensagens;
+        }
+
     }
 
 }
