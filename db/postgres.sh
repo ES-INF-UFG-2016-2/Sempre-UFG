10613af5214f49a043ab85128e622dc770489a31
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-10-31 18:24:59 -0200

    Altera lugar dos scripts do postgres

--- db/postgres.sh
+++ db/postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-psql -h localhost -U postgres -a -f create-user-and-db.sql
+psql -h localhost -U postgres -a -f db/ddl/create-user-and-db.sql
 #cat create-user-and-db.sql | sudo su - postgres -c psql
 #TODO: separar no script que gera o banco de dados e no que popula
 
