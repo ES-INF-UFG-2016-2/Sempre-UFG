16db8ff27d3c41e78843a88413a88bfc807e8d33
Johnathan Gomes Lima <johnathan_gomes@outlook.com>
2016-11-05 14:43:05 -0200

    Fixando inconsistências na implementação dos testes.

---
+++ src/test/java/br/ufg/inf/stubs/ConsPredefDAOStub.java
@@ -0,0 +1,7 @@
+package br.ufg.inf.stubs;
+
+public class ConsPredefDAOStub {
+    public void saveQueryMetada(ConsPredefStub cps) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/br/ufg/inf/stubs/ConsPredefStub.java
@@ -0,0 +1,23 @@
+package br.ufg.inf.stubs;
+
+import java.util.Date;
+
+public class ConsPredefStub {
+
+    private String siglaConsulta;
+    private String visibilidade;
+    private boolean expressaoBooleana;
+    private Date dataUltimaModificacao;
+    private Date dataUltimaExecucao;
+    private String idUsuarioResponsavel;
+
+    public ConsPredefStub(String siglaConsulta, String visibilidade, boolean expressaoBooleana, Date dataUltimaModificacao, Date dataUltimaExecucao, String idUsuarioResponsavel) {
+        this.siglaConsulta = siglaConsulta;
+        this.visibilidade = visibilidade;
+        this.expressaoBooleana = expressaoBooleana;
+        this.dataUltimaModificacao = dataUltimaModificacao;
+        this.dataUltimaExecucao = dataUltimaExecucao;
+        this.idUsuarioResponsavel = idUsuarioResponsavel;
+    }
+
+}
