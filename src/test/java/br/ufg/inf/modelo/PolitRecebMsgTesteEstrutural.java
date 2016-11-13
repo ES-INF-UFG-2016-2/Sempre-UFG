6306dbff5d7a45d1390ba8c233328bd4ca6bfbb7
rafaelccunha <rafaelccunha@gmail.com>
2016-11-13 17:32:22 -0200

    Define classe de testes estruturais

---
+++ src/test/java/br/ufg/inf/modelo/PolitRecebMsgTesteEstrutural.java
@@ -0,0 +1,35 @@
+package br.ufg.inf.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PolitRecebMsgTesteEstrutural {
+
+	@Test
+	public void testDefinePolitRecebMsg() {
+	}
+	
+	@Test
+	public void testAlteraPolitRecebMsg() {
+	}
+	
+	@Test
+	public void testPolitRecebMsgNull() {
+	}
+	
+	@Test
+	public void testRetornaPolitRecebMsg() {
+	}
+	
+	@Test
+	public void testPolitRecebMsgDefault() {
+	}
+	
+	@Test
+	public void testPolitRecebMsgInvalida() {
+	}
+	
+	
+
+}
