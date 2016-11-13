1d428f6ba2ca93d2c1ca05be3a054798c0d48b75
rafaelccunha <rafaelccunha@gmail.com>
2016-11-13 21:29:36 -0200

    Define possiveis casos de teste estrutural

--- src/test/java/br/ufg/inf/modelo/PolitRecebMsgTesteEstrutural.java
+++ src/test/java/br/ufg/inf/modelo/PolitRecebMsgTesteEstrutural.java
@@ -2,34 +2,68 @@ package br.ufg.inf.modelo;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import br.ufg.inf.enums.PoliticaRecebimentoMensagens;
 
 public class PolitRecebMsgTesteEstrutural {
 
+	@BeforeClass
+	public static void defineUsuario() {
+
+		Usuario usuario = new Usuario();
+
+	}
+
 	@Test
 	public void testDefinePolitRecebMsg() {
+
 	}
-	
+
 	@Test
 	public void testAlteraPolitRecebMsg() {
 	}
-	
+
 	@Test
 	public void testPolitRecebMsgNull() {
+
+		Usuario usuarioDefault = new Usuario();
+		String politica = "politica";
+
+		try {
+			usuarioDefault.setTipoDivulgacao(null);
+			// fail("Politica null foi aprovada.");
+		} catch (Exception e) {
+
+			e.getMessage();
+
+		}
+
 	}
-	
+
 	@Test
 	public void testRetornaPolitRecebMsg() {
 	}
-	
+
 	@Test
-	public void testPolitRecebMsgDefault() {
+	public void testPolitRecebMsgDefault() throws Exception {
+
 	}
-	
+
 	@Test
 	public void testPolitRecebMsgInvalida() {
-	}
-	
-	
 
+		Usuario usuarioDefault = new Usuario();
+		String politica = "politica";
+
+		try {
+			usuarioDefault.setTipoDivulgacao(PoliticaRecebimentoMensagens.valueOf("jkahdsf"));
+			fail("Politica invalida foi aprovada.");
+		} catch (Exception e) {
+
+			assertEquals(java.lang.IllegalArgumentException.class, e.getClass());
+		}
+
+	}
 }
