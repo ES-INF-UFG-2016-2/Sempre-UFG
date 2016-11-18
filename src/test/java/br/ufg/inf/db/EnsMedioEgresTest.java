14129d1b970b24b779221fd90e877f48293dab91
RenanOfugi <renangyn2010@hotmail.com>
2016-11-18 01:54:52 -0200

    Teste Inserção de dados de egresso e ensino médio:  * status: Implementado

--- src/test/java/br/ufg/inf/db/EnsMedioEgresTest.java
+++ src/test/java/br/ufg/inf/db/EnsMedioEgresTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,19 +14,24 @@ import org.junit.Test;
 
 import br.ufg.inf.db.auxiliar.AuxiliaInsercao;
 import br.ufg.inf.enums.Sexo;
+import br.ufg.inf.enums.TipoInstituicao;
 import br.ufg.inf.enums.VisibilidadeDados;
+import br.ufg.inf.excecoes.DadosBDInvalidosException;
 import br.ufg.inf.excecoes.ErroConexaoException;
+import br.ufg.inf.modelo.Egresso;
 
 public class EnsMedioEgresTest {
 
 	private Connection connection;
 	private AuxiliaInsercao auxiliar;
+	private Egresso egresso;
 
 	@Before
 	public void PreparaTeste() {
 
 		connection = ConexaoBanco.getConnection();
 		auxiliar = new AuxiliaInsercao();
+		egresso = new Egresso();
 	}
 
 	@After
@@ -37,16 +45,42 @@ public class EnsMedioEgresTest {
 	}
 
 	@Test
-	public void InserirDadosIdeiais() {
+	public void testeInserirDadosIdeiaisEgresso() {
 
+		Date dateformat;
 		String sql = "insert into egresso"
 				+ " (nome, nome_mae, data_nascimento, foto_principal, foto_adicionais, visibilidade, sexo, id_localizacao)"
 				+ " values (?,?,?,?,?,?,?,?)";
 
-		boolean inseriu = auxiliar.insereEgresso(connection, sql, "Fulano", "Ciclana", "12/05/1972", null, null,
-				VisibilidadeDados.PUBLICO.toString(), Sexo.MASCULINO.toString());
+		try {
+			String data_nascimento = "12/05/1972";
+			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+			dateformat = format.parse(data_nascimento);
+		} catch (ParseException e) {
+			throw new DadosBDInvalidosException("Parse de data não permitido");
+		}
 
-		assertTrue("Inserido com sucesso", inseriu);
+		egresso.setNome("Jose da Silva");
+		egresso.setNome_mae("Maria Aparecida da Silva");
+		egresso.setData_nascimento(dateformat);
+		egresso.setFoto_principal(null);
+		egresso.setFotos_adicionais(null);
+		egresso.setFotos_adicionais(null);
+		egresso.setVisibilidade(VisibilidadeDados.PUBLICO);
+		egresso.setSexo(Sexo.MASCULINO);
+
+		boolean inseriu = auxiliar.insereEgresso(connection, sql, egresso);
+
+		assertTrue("Egresso inserido com sucesso", inseriu);
 	}
 
+	@Test
+	public void testeInserirDadosIdeaisEnsinoMedio() {
+
+		String sql = "insert into ensmedioegres" + " (nome_iem, tipo_iem)" + " values (?,?)";
+
+		boolean inseriu = auxiliar.insereEnsinoMedio(connection, sql, "Colegio Omega", TipoInstituicao.Particular);
+
+		assertTrue("Dados Ensino Medio Inserido com sucesso", inseriu);
+	}
 }
--- src/test/java/br/ufg/inf/db/auxiliar/AuxiliaInsercao.java
+++ src/test/java/br/ufg/inf/db/auxiliar/AuxiliaInsercao.java
@@ -1,45 +1,56 @@
 package br.ufg.inf.db.auxiliar;
 
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import br.ufg.inf.excecoes.DadosBDInvalidosException;
+import br.ufg.inf.enums.TipoInstituicao;
+import br.ufg.inf.modelo.Egresso;
 
 public class AuxiliaInsercao {
-	
-	public boolean insereEgresso(Connection connection, String sql, String nome, String nome_mae,
-			 String data_nascimento, byte[] foto_principal, byte[] foto_adicional, String visibilidade,
-			 String sexo){
-		
+
+	public boolean insereEgresso(Connection connection, String sql, Egresso egresso) {
+
 		try {
-			
-			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-			Date dateformat = format.parse(data_nascimento);
 
 			PreparedStatement stmt = connection.prepareStatement(sql);
-			
-			stmt.setString(1, nome);
-			stmt.setString(2, nome_mae);
-			stmt.setDate(3, (java.sql.Date) dateformat);
-			stmt.setBytes(4, foto_principal);
-			stmt.setBytes(5, foto_adicional);
-			stmt.setString(6, visibilidade);
-			stmt.setString(7, sexo);
-			
+
+			stmt.setString(1, egresso.getNome());
+			stmt.setString(2, egresso.getNome_mae());
+			stmt.setDate(3, (java.sql.Date) egresso.getData_nascimento());
+			stmt.setBlob(4, (Blob) egresso.getFoto_principal());
+			stmt.setBlob(5, (Blob) egresso.getFotos_adicionais());
+			stmt.setString(6, egresso.getVisibilidade().toString());
+			stmt.setString(7, egresso.getSexo().toString());
+
 			stmt.execute();
 			stmt.close();
-			
+
 			return true;
-			
+
 		} catch (SQLException e) {
-			throw new DadosBDInvalidosException("Estrutura SQL errada");
-		} catch (ParseException e) {
-			throw new DadosBDInvalidosException("Erro transformacao: String para Date");
+			return false;
 		}
-		
+
+	}
+
+	public boolean insereEnsinoMedio(Connection connection, String sql, String nome, TipoInstituicao instituicao) {
+
+		try {
+
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			stmt.setString(1, nome);
+			stmt.setString(2, instituicao.toString());
+
+			stmt.execute();
+			stmt.close();
+
+			return true;
+
+		} catch (SQLException e) {
+			return false;
+		}
 	}
 }
