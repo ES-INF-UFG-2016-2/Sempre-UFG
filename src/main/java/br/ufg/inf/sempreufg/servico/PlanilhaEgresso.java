e3c543528325501f3a844ba77e151c0b702fcd21
rccunha <rafaelccunha@live.com>
2016-11-21 21:39:16 -0200

    Cria classe PlanilhaEgresso.java

---
+++ src/main/java/br/ufg/inf/sempreufg/servico/PlanilhaEgresso.java
@@ -0,0 +1,51 @@
+package br.ufg.inf.sempreufg.servico;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class PlanilhaEgresso {
+
+	static String path_projeto = System.getProperty("user.dir");
+
+	
+	
+	public static void salvaPlaniEgres(File planilha, String nome_plani,
+								String path_fornecido){
+	
+		String path_planilha = path_projeto+File.separator+path_fornecido;
+		File path_rename = new File(path_planilha);
+		path_rename.mkdir();
+		
+		if(	planilha.renameTo(new File(path_planilha+File.separator+nome_plani+".ods"))){
+			System.out.println("sucesso");
+		}
+				
+		
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(planilha));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	public static void main (String args[]) {
+		
+		
+		
+		String path_fornecido = "planilhas";
+		File planilha = new File("planilha.ods");
+		salvaPlaniEgres(planilha, "Planilha Egressos", path_fornecido);
+		
+		
+		
+		
+	}
+	
+
+}
