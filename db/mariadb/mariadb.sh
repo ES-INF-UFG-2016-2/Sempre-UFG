20dc30b0fe38342d8a539b326ad2dc670d08ceb8
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-11-21 21:30:59 -0200

    Adiciona script de configuração do MariaDB

---
+++ db/mariadb/mariadb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Script para popular o mariadb com os dados necessarios para rodar os testes
+
+set -e
+
+echo "Exectando em : $(pwd)"
+
+export MARIAFOLDER='db/mariadb'
+export MYSQL_DATABASE='sempreufg'
+export MYSQL_USER='sempreufg'
+export MYSQL_PASSWORD='sempreufg'
+
+mysql -h localhost < $MARIAFOLDER/ddl/cria-usuario-e-db.sql
