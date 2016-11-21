0a9dd8275f0db34fa0b71dd10debb3a92b5503ce
Gabriela Aimée <gabrielaimeeg@hotmail.com>
2016-11-21 21:16:22 -0200

    Implementados métodos específicos para limpar as tabelas em cada banco e estabelecer conexão.

--- src/test/java/br/ufg/inf/db/MetaEgresDDLTestMariaDB.java
+++ src/test/java/br/ufg/inf/db/MetaEgresDDLTestMariaDB.java
@@ -1,17 +1,12 @@
-package java.br.ufg.inf.db;;
+package java.br.ufg.inf.db;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Test;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MetaEgresDDLTestMariaDB {
-
-    private static Connection conexaoBD;
+public class MetaEgresDDLTestMariaDB extends MetaEgresDDLTest {
 
     @BeforeClass
     public static void setupTest() {
@@ -29,117 +24,11 @@ public class MetaEgresDDLTestMariaDB {
     public void limpaTabelas() {
         try {
             executaSqlComStatement("SET FOREIGN_KEY_CHECKS=0;");
-            executaSqlComStatement("TRUNCATE TABLE ENTIDADE;");
-            executaSqlComStatement("TRUNCATE TABLE ATRIBUTO;");
-            executaSqlComStatement("TRUNCATE TABLE DESVIARPARA;");
+            executaSqlComStatement("TRUNCATE TABLE entidade;");
+            executaSqlComStatement("TRUNCATE TABLE atributo;");
             executaSqlComStatement("SET FOREIGN_KEY_CHECKS=1;");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
-    @Test
-    public void testaConexaoComMariaDB() {
-        Assert.assertNotNull(conexaoBD);
-    }
-
-    @Test
-    public void testaSeTabelaEntidadeExiste() {
-
-    }
-
-    @Test
-    public void testaSeTabelaAtributoExiste() {
-
-    }
-
-    @Test
-    public void testInserirNaTabelaEntidadeComSucesso() {
-
-    }
-
-    @Test
-    public void testInserirNaTabelaEntidadeComMesmoIdentificadorDeveLancarExcecao() {
-
-    }
-
-    @Test
-    public void testInsercaoNaTabelaEntidadeSemCamposObrigatoriosELancaExcecao() {
-
-    }
-
-    @Test
-    public void testBuscaEntidadeInexistenteELancaExcecao() {
-
-    }
-
-    @Test
-    public void testInserirMultiplosDadosComSucessoNaTabelaEntidade() {
-
-    }
-
-    @Test
-    public void testInserirDadoComTipoInvalidoDeveLancarExcecaoNaTebalaEntidade() {
-
-    }
-
-    @Test
-    public void testInserirDadoComIdentificadorJaExistenteNaTabelaEntidade() {
-
-    }
-
-    @Test
-    public void testInserirNaTabelaAtributoComSucesso() {
-
-    }
-
-    @Test
-    public void testInserirNaTabelaAtributoComMesmoIdentificadorDeveLancarExcecao() {
-
-    }
-
-    @Test
-    public void testInsercaoNaTabelaAtributoSemCamposObrigatoriosELancaExcecao() {
-
-    }
-
-    @Test
-    public void testBuscaAtributoInexistenteELancaExcecao() {
-
-    }
-
-    @Test
-    public void testInserirMultiplosDadosComSucessoNaTabelaAtributo() {
-
-    }
-
-    @Test
-    public void testInserirDadoComTipoInvalidoDeveLancarExcecaoNaTebalaAtributo() {
-
-    }
-
-    @Test
-    public void testInserirDadoComIdentificadorJaExistenteNaTabelaAtributo() {
-
-    }
-
-    private static boolean verificaSeTabelaExiste(String nomeTabela) {
-        return true;
-    }
-
-    private static void inserirNaTabelaAtributo(int idUsuario, String email, String senha, String nomeSocial, int CPF) throws SQLException {
-
-    }
-
-    private static void inserirNaTabelaEntidade(String sigla, String nomeSistema, String tipo, String descricao,
-                                                String valor) throws SQLException {
-
-    }
-
-
-    private static void executaSqlComStatement(String sql) throws SQLException {
-
-    }
-
-
 }
--- src/test/java/br/ufg/inf/db/MetaEgresDDLTestPostgres.java
+++ src/test/java/br/ufg/inf/db/MetaEgresDDLTestPostgres.java
@@ -1,23 +1,18 @@
 package java.br.ufg.inf.db;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Test;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MetaEgresDDLTestPostgres {
-
-    private static Connection conexaoBD;
+public class MetaEgresDDLTestPostgres extends MetaEgresDDLTest {
 
     @BeforeClass
     public static void setupTest() {
         try {
             Class.forName("org.postgresql.Driver");
-            conexaoBD = DriverManager.getConnection("jdbc:mysql://localhost:3306/sempreUFG", "root", "admin");
+            conexaoBD = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sempreufg", "root", "admin");
         } catch (ClassNotFoundException e) {
             System.out.println("Classe do Postgres não encontrada.");
         } catch (SQLException e) {
@@ -27,109 +22,10 @@ public class MetaEgresDDLTestPostgres {
 
     @After
     public void limpaTabelas() {
-
-    }
-
-    @Test
-    public void testaConexaoComPostgres() {
-        Assert.assertNotNull(conexaoBD);
-    }
-
-    @Test
-    public void testaSeTabelaEntidadeExiste() {
-
-    }
-
-    @Test
-    public void testaSeTabelaAtributoExiste() {
-
-    }
-
-    @Test
-    public void testInserirNaTabelaEntidadeComSucesso() {
-
-    }
-
-    @Test
-    public void testInserirNaTabelaEntidadeComMesmoIdentificadorDeveLancarExcecao() {
-
-    }
-
-    @Test
-    public void testInsercaoNaTabelaEntidadeSemCamposObrigatoriosELancaExcecao() {
-
-    }
-
-    @Test
-    public void testBuscaEntidadeInexistenteELancaExcecao() {
-
-    }
-
-    @Test
-    public void testInserirMultiplosDadosComSucessoNaTabelaEntidade() {
-
-    }
-
-    @Test
-    public void testInserirDadoComTipoInvalidoDeveLancarExcecaoNaTebalaEntidade() {
-
-    }
-
-    @Test
-    public void testInserirDadoComIdentificadorJaExistenteNaTabelaEntidade() {
-
-    }
-
-    @Test
-    public void testInserirNaTabelaAtributoComSucesso() {
-
-    }
-
-    @Test
-    public void testInserirNaTabelaAtributoComMesmoIdentificadorDeveLancarExcecao() {
-
-    }
-
-    @Test
-    public void testInsercaoNaTabelaAtributoSemCamposObrigatoriosELancaExcecao() {
-
-    }
-
-    @Test
-    public void testBuscaAtributoInexistenteELancaExcecao() {
-
-    }
-
-    @Test
-    public void testInserirMultiplosDadosComSucessoNaTabelaAtributo() {
-
-    }
-
-    @Test
-    public void testInserirDadoComTipoInvalidoDeveLancarExcecaoNaTebalaAtributo() {
-
-    }
-
-    @Test
-    public void testInserirDadoComIdentificadorJaExistenteNaTabelaAtributo() {
-
-    }
-
-    private static boolean verificaSeTabelaExiste(String nomeTabela) {
-        return true;
-    }
-
-    private static void inserirNaTabelaAtributo(int idUsuario, String email, String senha, String nomeSocial, int CPF) throws SQLException {
-
-    }
-
-    private static void inserirNaTabelaEntidade(String sigla, String nomeSistema, String tipo, String descricao,
-                                                String valor) throws SQLException {
-
-    }
-
-
-    private static void executaSqlComStatement(String sql) throws SQLException {
-
+        try {
+            executaSqlComStatement("TRUNCATE TABLE entidade CASCADE;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
