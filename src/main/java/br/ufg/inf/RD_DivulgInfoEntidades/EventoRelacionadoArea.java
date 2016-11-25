20f9695a4ca969a211dde4acd1331f4dc44de594
Leonardo Kalyn <Leonardo.Kalyn@gmail.com>
2016-11-25 14:40:11 -0200

    Adicionado:: Classes de Entidades de RD-DivulgInfo
    AreaDeConhecimento
    Evento
    EventoRelacionadostancia
    EventoRelacionadoArea
    InstanciaAdministrativa

---
+++ src/main/java/br/ufg/inf/RD_DivulgInfoEntidades/EventoRelacionadoArea.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.RD_DivulgInfoEntidades;
+
+/**
+ *
+ * @author Kalyn
+ */
+public class EventoRelacionadoArea {
+    private String EVENTO;
+    private String AREA_CONHECIMENTO;
+
+    public String getEVENTO() {
+        return EVENTO;
+    }
+
+    public void setEVENTO(String EVENTO) {
+        this.EVENTO = EVENTO;
+    }
+
+    public String getAREA_CONHECIMENTO() {
+        return AREA_CONHECIMENTO;
+    }
+
+    public void setAREA_CONHECIMENTO(String AREA_CONHECIMENTO) {
+        this.AREA_CONHECIMENTO = AREA_CONHECIMENTO;
+    }
+    
+    
+}
