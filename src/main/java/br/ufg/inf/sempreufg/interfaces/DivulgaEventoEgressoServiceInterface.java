0d0b3cd75a1d31b48a77dbf56f7a5a4457085392
LucasFerreiraRodrigue <lucas_f_rodrigues@hotmail.com>
2016-12-01 21:45:13 -0200

    Refatoração da Interface

--- src/main/java/br/ufg/inf/sempreufg/interfaces/DivulgaEventoEgressoServiceInterface.java
+++ src/main/java/br/ufg/inf/sempreufg/interfaces/DivulgaEventoEgressoServiceInterface.java
@@ -2,7 +2,11 @@ package br.ufg.inf.sempreufg.interfaces;
 
 import br.ufg.inf.sempreufg.modelo.CursoUFG;
 import br.ufg.inf.sempreufg.modelo.Evento;
+import br.ufg.inf.sempreufg.modelo.Usuario;
 
 public interface DivulgaEventoEgressoServiceInterface {
 	boolean divulgaEventoDoCursoParaEgresso(Evento evento, CursoUFG cursoUFG);
+	boolean usuarioRecebeuHoje(Usuario usuario);
+	boolean usuarioRecebeuSemanal(Usuario usuario);
+	boolean usuarioRecebeuMensal(Usuario usuario);
 }
