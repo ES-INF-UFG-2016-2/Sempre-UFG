ce79491586e148b7b27bd61c8222917555d2ab8e
kleudson <kleudson20@hotmail.com>
2016-12-02 00:35:48 -0200

    Implementado Testes Funcionais RNF ConsulLog

---
+++ src/main/java/br/ufg/inf/sempreufg/dao/LogDao.java
@@ -0,0 +1,132 @@
+package br.ufg.inf.sempreufg.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufg.inf.sempreufg.modelo.Log;
+/**
+ * 
+ * @author Kleudson
+ * @version 1.0
+ */
+public class LogDao {
+
+    private Connection con;
+
+    public LogDao(Connection con) throws NullPointerException {
+        if (con == null) {
+            throw new NullPointerException("Não é permitido informar parâmetro de conexão nulo.");
+        }
+        this.con = con;
+    }
+
+    /**
+     * Adiciona um log ao banco de dados
+     * @param log 
+     */
+    public void salvarLog(Log log) {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append("insert into log (id_log, mensagem, usuario, tipo) ");
+        sql.append("values(?, ?, ?, ?)");
+
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = this.con.prepareStatement(sql.toString());
+            preparedStatement.setInt(1, log.getIdLog());
+            preparedStatement.setString(2, log.getMensagem());
+            preparedStatement.setString(3, log.getUsuario());
+            preparedStatement.setInt(4, log.getTipoLog());
+            preparedStatement.execute();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Remove um log do banco de dados
+     * @param log 
+     */
+    public void removerLog(Log log) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("delete from log where id_log = ?");
+
+        PreparedStatement preparedStatement;
+        try {
+
+            preparedStatement = this.con.prepareStatement(sql.toString());
+            preparedStatement.setInt(1, log.getIdLog());
+            preparedStatement.execute();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retorna um log através do ID
+     * 
+     * @param idLog
+     * @return 
+     */
+    public Log consultaLog(int idLog) {
+        try {
+            Log log = null;
+            StringBuilder sql = new StringBuilder();
+            sql.append("select * from log where id_log = ? ");                    
+            PreparedStatement preparedStatement;
+
+            preparedStatement = this.con.prepareStatement(sql.toString());
+            preparedStatement.setInt(1, idLog);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                log = new Log();
+                log.setIdLog(resultSet.getInt("id_log"));
+                log.setMensagem(resultSet.getString("mensagem"));
+                log.setTipoLog(resultSet.getInt("tipo"));
+                log.setUsuario(resultSet.getString("usuario"));                
+            }
+
+            return log;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Log> consultaLogs() {
+        List<Log> logs = new ArrayList<Log>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("select * from log");              
+
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = this.con.prepareStatement(sql.toString());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Log log = new Log();
+                log.setIdLog(resultSet.getInt("id_log"));
+                log.setMensagem(resultSet.getString("mensagem"));
+                log.setTipoLog(resultSet.getInt("tipo"));
+                log.setUsuario(resultSet.getString("usuario"));
+
+                logs.add(log);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return logs;
+    }
+
+}
---
+++ src/test/java/br/ufg/inf/sempreufg/dao/LogDaoTest.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.sempreufg.dao;
+
+import br.ufg.inf.sempreufg.factory.ConnectionFactoryPostegres;
+import br.ufg.inf.sempreufg.modelo.Log;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kleudson
+ * @version 1.0
+ */
+public class LogDaoTest {
+
+    private Connection con;
+    private LogDao logDao;
+
+    public LogDaoTest() {
+    }
+
+    @Before
+    public void setUp() {
+        try {
+            //Inicializa variáveis antes do teste
+            con = new ConnectionFactoryPostegres().getConnection();
+            logDao = new LogDao(con);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LogDaoTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(LogDaoTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            con.close();
+        } catch (Exception e) {
+        }
+
+    }
+
+    @Test
+    public void crudLog() {
+        Log log = mockLog();
+
+        logDao.salvarLog(log);
+
+        Log logSalvo = logDao.consultaLog(log.getIdLog());
+
+        //Verifica se o método de inserção e seleção estão funcionando
+        assertEquals(log, logSalvo);
+        
+        logDao.removerLog(logSalvo);
+
+        logSalvo = logDao.consultaLog(logSalvo.getIdLog());
+
+        //Verifica remoção
+        assertNull(logSalvo);
+
+    }
+
+   
+
+    private Log mockLog() {
+        Log log = new Log();
+        log.setIdLog(1);
+        log.setMensagem("Mensagem");
+        log.setTipoLog(1);
+        log.setUsuario("admin");
+
+        return log;
+    }
+}
