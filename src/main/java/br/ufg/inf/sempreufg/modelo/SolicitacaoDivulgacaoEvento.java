e5a9cf87bcb5f4f4edfe6725e94c140fd4387682
Johnathan Gomes Lima <johnathan_gomes@outlook.com>
2016-12-05 16:26:21 -0200

    Removendo Entidade Inconsistente SolicitacaoDivulgacao
    Outro teste foi comentado (TesteAvalSolicDivulgEvent) para garantir que
    o projeto compila de acordo com a refatoração das classes.

--- src/main/java/br/ufg/inf/sempreufg/modelo/SolicitacaoDivulgacaoEvento.java
+++ src/main/java/br/ufg/inf/sempreufg/modelo/SolicitacaoDivulgacaoEvento.java
@@ -2,16 +2,26 @@ package br.ufg.inf.sempreufg.modelo;
 
 public class SolicitacaoDivulgacaoEvento {
 
+    private int id;
     private Evento evento;
     private Usuario usuario;
     private boolean aprovado;
 
     public SolicitacaoDivulgacaoEvento(){}
 
-    public SolicitacaoDivulgacaoEvento(Evento evento, Usuario usuario) {
+    public SolicitacaoDivulgacaoEvento(int id, Evento evento, Usuario usuario) {
+        this.id = id;
         this.evento = evento;
         this.usuario = usuario;
         this.aprovado = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Evento getEvento() {
