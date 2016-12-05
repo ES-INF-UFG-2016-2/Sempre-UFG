c72f2891de34ff80043e799e8a60b6fb98c40ad5
rafaelccunha <rafaelccunha@gmail.com>
2016-12-04 22:32:53 -0200

    Deleta classes desnecessarias
    Classes deletadas:
    - Campo
    - Linha
    - Tabela

--- src/main/java/br/ufg/inf/sempreufg/servico/PlanilhaResultados.java
+++ src/main/java/br/ufg/inf/sempreufg/servico/PlanilhaResultados.java
@@ -17,31 +17,10 @@ import javax.swing.table.TableModel;
 import org.jopendocument.dom.OOUtils;
 import org.jopendocument.dom.spreadsheet.SpreadSheet;
 
-import br.ufg.inf.sempreufg.dados.Campo;
-import br.ufg.inf.sempreufg.dados.Linha;
-import br.ufg.inf.sempreufg.dados.Tabela;
 
 public class PlanilhaResultados {
 
 	private Object[][] tabela = null;
-	String caminho_arq;
-	String nome_arq;
-
-	public static void main(String args[]) {
-
-		final Object[][] data = new Object[6][1];
-		data[0] = new Object[] { "Nome", "Cpf", "Cidade", "Idade", "Ano Graduacao" };
-		data[1] = new Object[] { "Rafael", "12313123123", "Goiânia", "22", "2010" };
-		data[2] = new Object[] { "Rafael", "12313123123", "Goiânia", "23", "2011" };
-		data[3] = new Object[] { "Rafael", "12313123123", "Goiânia", "24", "2012" };
-		data[4] = new Object[] { "Rafael", "12313123123", "Goiânia", "25", "2013" };
-		data[5] = new Object[] { "Rafael", "12313123123", "Goiânia", "26", "2014" };
-
-		String path = "C:\\Users\\Rafael\\Desktop\\planilhas";
-		String nome = "Resultados";
-		new PlanilhaResultados().salvaParametros(data, path, nome);
-
-	}
 
 	public void salvaParametros(Object[][] tabela, String caminho, String nome) {
 
@@ -71,9 +50,6 @@ public class PlanilhaResultados {
 		for (int i = 0; i < tabela.length; i++) {
 
 			for (int j = 0; j < tabela[i].length; j++) {
-
-				System.out.println(i + "," + j);
-				System.out.println(tabela[i][j]);
 
 				if (i == tabela.length - 1) {
 
