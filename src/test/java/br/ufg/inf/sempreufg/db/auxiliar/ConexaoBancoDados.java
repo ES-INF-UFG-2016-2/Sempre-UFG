928f3b941c8f434d12f61abb1e15c513773d708c
RenanOfugi <renangyn2010@hotmail.com>
2016-12-12 20:18:59 -0200

    Refatorando classe de Banco de Dados

---
+++ src/test/java/br/ufg/inf/sempreufg/db/auxiliar/ConexaoBancoDados.java
@@ -0,0 +1,41 @@
+package br.ufg.inf.sempreufg.db.auxiliar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBancoDados {
+
+	public static Connection conn = null;
+
+	public static Connection getConnection() {
+		if (conn != null)
+			return conn;
+
+		return getConnection("sempreufg");
+
+	}
+
+	public static Connection getConnection(String username) {
+
+		String drivepostgre = "org.postgresql.Driver";
+		String drivemariadb = "org.mariadb.jdbc.Driver";
+		
+		String headpostgre = "jdbc:postgresql://localhost:5432";
+		String headmariadb = "jdbc:mariadb://localhost:3306";
+		
+		try {
+			Class.forName(drivepostgre);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			conn = DriverManager.getConnection(headmariadb + "/sempreufg", username, "sempreufg");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return conn;
+		}
+		return conn;
+	}
+}
