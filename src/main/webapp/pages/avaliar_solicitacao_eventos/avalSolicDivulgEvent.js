d7b3c19f2f996fc34ae35dadcfaff40645afbc3b
RenanOfugi <renangyn2010@hotmail.com>
2016-12-11 23:47:54 -0200

    Design de AvalSolicDivulgEvento realizado  * status: finalizado

--- src/main/webapp/pages/avaliar_solicitacao_eventos/avalSolicDivulgEvent.js
+++
@@ -1,6 +0,0 @@
-$('#eventoModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget)
-  var recipient = button.data('whatever') 
-  var modal = $(this)
-  modal.find('.modal-body input').val(recipient)
-})
