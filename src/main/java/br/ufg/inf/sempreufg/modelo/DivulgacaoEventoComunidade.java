f9922021d5262c3f27618410aa881a689f0ced91
Breno Fernandes Daniel de Carvalho <brenofdc@gmail.com>
2016-12-12 21:21:18 -0200

    Implementando DAO e fazendo ajustes na entidade DivulgacaoEventoComunidade.

--- src/main/java/br/ufg/inf/sempreufg/modelo/DivulgacaoEventoComunidade.java
+++ src/main/java/br/ufg/inf/sempreufg/modelo/DivulgacaoEventoComunidade.java
@@ -1,18 +1,29 @@
 package br.ufg.inf.sempreufg.modelo;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+// Não há DDL para esta entidade! @Table(name = "")
 public class DivulgacaoEventoComunidade {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="<id_row>")
+    @SequenceGenerator(name="<id_row>", initialValue=1, sequenceName="DIVULGACAO_EVENTO_COMUNIDADE_ID_SEQUENCE", allocationSize=1)
+    private int id;
     private Evento evento;
     private List<Usuario> usuarios;
 
-    public DivulgacaoEventoComunidade(Evento evento) {
+    public DivulgacaoEventoComunidade(){}
+
+    public DivulgacaoEventoComunidade(Evento evento, List<Usuario> usuarios) {
         this.evento = evento;
+        this.usuarios = usuarios;
     }
 
     public List<Usuario> getUsuarios() {
-        return usuarios;
+        return new ArrayList<>(this.usuarios);
     }
 
     public void setUsuarios(List<Usuario> usuarios) {
