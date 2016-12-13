0b5ff3518eb15973d7de814f78a3eeb5a31eb56f
Rafael <rafaelcdmm@gmail.com>
2016-12-13 17:28:30 -0200

    Desfazendo alteração anterior
    Desfazendo alteração anterior na classe ExecultarConsultasMock.java

--- src/main/java/br/ufg/inf/sempreufg/servico/ExecultarConsultasMock.java
+++ src/main/java/br/ufg/inf/sempreufg/servico/ExecultarConsultasMock.java
@@ -26,6 +26,7 @@ public class ExecultarConsultasMock {
 									  NomeMae,
 									  new Date(),
 									  sexo,
+                                      "",
 									  null,
 									  null,
 									  visibilidadeDados,
