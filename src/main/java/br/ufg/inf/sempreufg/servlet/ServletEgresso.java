d54971375321cf18d48ffc623120ca05043c0007
Rafael <rafaelcdmm@gmail.com>
2016-12-13 16:45:01 -0200

    Finalizando implementação da ServletEgresso
    Finalizando implementação da ServletEgresso

--- src/main/java/br/ufg/inf/sempreufg/servlet/ServletEgresso.java
+++ src/main/java/br/ufg/inf/sempreufg/servlet/ServletEgresso.java
@@ -12,12 +12,76 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class ServletEgresso extends HttpServlet{
+public class ServletEgresso extends HttpServlet {
 
-    static EgressoService egressoService;
+    private EgressoService egressoService;
 
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean egressoValido = validarEgresso(request);
+
+        if (egressoValido) {
+            enviarEgresso(request, response);
+        } else {
+            enviarErroDeValidacao(response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            atualizarEgresso(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean validarEgresso(HttpServletRequest request) throws ServletException, IOException {
+
+        ValidadorEgresso validadorEgresso = new ValidadorEgresso();
+        int id = Integer.valueOf(request.getParameter("id"));
+
+        return validadorEgresso.validaEgresso(id);
+    }
+
+    private void enviarEgresso(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        response.setContentType("application/json");
+
+        JSONObject egressoAsJson = new JSONObject();
+
+        try (PrintWriter out = response.getWriter()) {
+
+            int id = Integer.valueOf(request.getParameter("id"));
+
+            egressoService = new EgressoService();
+            Egresso egresso = egressoService.getEgresso(id);
+            egressoAsJson = egresso.toJson();
+
+            out.print(egressoAsJson);
+            out.flush();
+        }
+    }
+
+    private void enviarErroDeValidacao(HttpServletResponse response)
+        throws ServletException, IOException {
 
         response.setContentType("application/json");
 
@@ -25,54 +89,25 @@ public class ServletEgresso extends HttpServlet{
 
         try (PrintWriter out = response.getWriter()) {
 
-            int id = Integer.valueOf(request.getParameter("id"));
-
-            ValidadorEgresso validadorEgresso = new ValidadorEgresso();
-            boolean egressoExiste = validadorEgresso.validaEgresso(id);
-
-            if (egressoExiste) {
-                egressoService = new EgressoService();
-                Egresso egresso = egressoService.getEgresso(id);
-
-                JSON.put("id", egresso.getId());
-                JSON.put("nome", egresso.getNome());
-                JSON.put("nome_mae", egresso.getNome_mae());
-                JSON.put("data_nascimento", egresso.getData_nascimento());
-                JSON.put("sexo", egresso.getSexo());
-                JSON.put("email_alternativo", egresso.getEmail_alternativo());
-                JSON.put("foto_principal", egresso.getFoto_principal());
-                JSON.put("fotos_adicionais", egresso.getFotos_adicionais());
-                JSON.put("visibilidade", egresso.getVisibilidade());
-                JSON.put("lista_historicosUFG", egresso.getLista_historicosUFG());
-                JSON.put("naturalidade", egresso.getNaturalidade());
-
-            } else try {
-                throw new Exception("Egresso não encontrado!");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
+            JSON.put("erro", "Egresso não Encontrado");
             out.print(JSON);
             out.flush();
         }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    private void atualizarEgresso(HttpServletRequest request, HttpServletResponse response)
+        throws Exception {
+
         response.setContentType("text/html");
 
         try (PrintWriter out = response.getWriter()) {
 
-            //capturar egresso do usuario e atualizar base de dados
-            String id = request.getParameter("id");
-
-
+            JSONObject egressoJson = new JSONObject(request.getParameter("egresso"));
+            Egresso egresso = Egresso.fromJson(egressoJson);
+            egressoService = new EgressoService();
+            egressoService.atualizarEgresso(egresso);
+            out.flush();
         }
-
-
-
-
     }
-
-
 }
