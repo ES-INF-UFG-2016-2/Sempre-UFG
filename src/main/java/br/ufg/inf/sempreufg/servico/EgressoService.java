744e62075c07f414bd410bf8a2e14f28644eba77
Rafael <rafaelcdmm@gmail.com>
2016-12-14 14:51:08 -0200

    Deixa identico arquivo para com develop
    Deixa identico arquivo para com develop

--- src/main/java/br/ufg/inf/sempreufg/servico/EgressoService.java
+++ src/main/java/br/ufg/inf/sempreufg/servico/EgressoService.java
@@ -1,6 +1,8 @@
 package br.ufg.inf.sempreufg.servico;
 
-import br.ufg.inf.sempreufg.dao.EgressoDAO;
+import java.io.InputStream;
+import java.util.*;
+
 import br.ufg.inf.sempreufg.enums.NomeCampos;
 import br.ufg.inf.sempreufg.enums.Sexo;
 import br.ufg.inf.sempreufg.enums.VisibilidadeDados;
@@ -8,99 +10,64 @@ import br.ufg.inf.sempreufg.modelo.Egresso;
 import br.ufg.inf.sempreufg.modelo.HistoricoUFG;
 import br.ufg.inf.sempreufg.modelo.LocalizacaoGeografica;
 
-import java.io.InputStream;
-import java.sql.SQLException;
-import java.util.*;
-
 
 public class EgressoService implements EgressoServiceInterface{
 
-    static EgressoDAO egressoDAO;
+	@Override
+	public Egresso converterXmlParaEgresso(InputStream content) {
+		Egresso egresso = new Egresso();
+		return egresso;
+	}
 
-    @Override
-    public void atualizarEgresso(Egresso egresso) {
-        egressoDAO = new EgressoDAO();
-        egressoDAO.atualizar(egresso);
-    }
+	@Override
+	public boolean egressoEValido(Egresso egresso) {
+		return false;
+	}
 
-    @Override
-    public Egresso getEgresso(int id) {
-        egressoDAO = new EgressoDAO();
-        Egresso egresso = null;
-        try {
-            egresso = egressoDAO.getById(id);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return egresso;
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoViaWebService() {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public void removerEgresso(int id) throws Exception {
-        try {
-            egressoDAO = new EgressoDAO();
-            egressoDAO.deletar(id);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new Exception("Não foi possível remover o Egresso!");
-        }
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoPeloPeriodoConclusaoCurso(Date dataInicial, Date dataFinal) {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public Egresso converterXmlParaEgresso(InputStream content) {
-        Egresso egresso = new Egresso();
-        return egresso;
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoPeloIdentificadorEgresso(List<Integer> identificadores) {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public boolean egressoEValido(Egresso egresso) {
-        return false;
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoPeloCurso(List<Integer> identificadores) {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public List<Egresso> buscarDadosEgressoViaWebService() {
-        return criarListaEgressoMock();
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoPelaUnidadeAcademica(List<Integer> identificadores) {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public List<Egresso> buscarDadosEgressoPeloPeriodoConclusaoCurso(Date dataInicial, Date dataFinal) {
-        return criarListaEgressoMock();
-    }
+	public List<Egresso> criarListaEgressoMock(){
+		List<Egresso> egressos = new ArrayList<Egresso>();
 
-    @Override
-    public List<Egresso> buscarDadosEgressoPeloIdentificadorEgresso(List<Integer> identificadores) {
-        return criarListaEgressoMock();
-    }
+		for (int i = 0; i < 10; i++) {
+			Egresso egresso = new Egresso("Everton Jose",
+										  "Maria",
+										  new Date(),
+										  Sexo.MASCULINO,
+										  "emailAlternativo@gmail.com",
+										  new BitSet(),
+										  new BitSet(),
+										  VisibilidadeDados.PUBLICO,
+										  new ArrayList<HistoricoUFG>(),
+                                                                                  new LocalizacaoGeografica());
+			egressos.add(egresso);
+		}
 
-    @Override
-    public List<Egresso> buscarDadosEgressoPeloCurso(List<Integer> identificadores) {
-        return criarListaEgressoMock();
-    }
-
-    @Override
-    public List<Egresso> buscarDadosEgressoPelaUnidadeAcademica(List<Integer> identificadores) {
-        return criarListaEgressoMock();
-    }
-
-    public List<Egresso> criarListaEgressoMock(){
-        List<Egresso> egressos = new ArrayList<Egresso>();
-
-        for (int i = 0; i < 10; i++) {
-            Egresso egresso = new Egresso("Everton Jose",
-                "Maria",
-                new Date(),
-                Sexo.MASCULINO,
-                "emailAlternativo@gmail.com",
-                new BitSet(),
-                new BitSet(),
-                VisibilidadeDados.PUBLICO,
-                new ArrayList<HistoricoUFG>(),
-                new LocalizacaoGeografica());
-            egressos.add(egresso);
-        }
-
-        return egressos;
-    }
+		return egressos;
+	}
 
     @Override
     public List<Egresso> consultarEgressoPorConsultaPreDefinida(String string) {
