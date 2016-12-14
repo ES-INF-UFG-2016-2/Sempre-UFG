abe9d4c0b5f03e008a3b7fc30330bb9c072b59bd
Rafael <rafaelcdmm@gmail.com>
2016-12-14 13:39:13 -0200

    Utiliza editorconfig
    Utiliza editorconfig

--- src/main/java/br/ufg/inf/sempreufg/servico/EgressoService.java
+++ src/main/java/br/ufg/inf/sempreufg/servico/EgressoService.java
@@ -24,10 +24,10 @@ public class EgressoService implements EgressoServiceInterface{
     }
 
     @Override
-    public Egresso getEgresso(int id) {
+    public Egresso getEgresso(int id){
         egressoDAO = new EgressoDAO();
         Egresso egresso = null;
-        try {
+        try{
             egresso = egressoDAO.getById(id);
         } catch (Exception e) {
             e.printStackTrace();
@@ -37,7 +37,7 @@ public class EgressoService implements EgressoServiceInterface{
 
     @Override
     public void removerEgresso(int id) throws Exception {
-        try {
+        try{
             egressoDAO = new EgressoDAO();
             egressoDAO.deletar(id);
         } catch (SQLException e) {
@@ -46,61 +46,61 @@ public class EgressoService implements EgressoServiceInterface{
         }
     }
 
-    @Override
-    public Egresso converterXmlParaEgresso(InputStream content) {
-        Egresso egresso = new Egresso();
-        return egresso;
-    }
+	@Override
+	public Egresso converterXmlParaEgresso(InputStream content) {
+		Egresso egresso = new Egresso();
+		return egresso;
+	}
 
-    @Override
-    public boolean egressoEValido(Egresso egresso) {
-        return false;
-    }
+	@Override
+	public boolean egressoEValido(Egresso egresso) {
+		return false;
+	}
 
-    @Override
-    public List<Egresso> buscarDadosEgressoViaWebService() {
-        return criarListaEgressoMock();
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoViaWebService() {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public List<Egresso> buscarDadosEgressoPeloPeriodoConclusaoCurso(Date dataInicial, Date dataFinal) {
-        return criarListaEgressoMock();
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoPeloPeriodoConclusaoCurso(Date dataInicial, Date dataFinal) {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public List<Egresso> buscarDadosEgressoPeloIdentificadorEgresso(List<Integer> identificadores) {
-        return criarListaEgressoMock();
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoPeloIdentificadorEgresso(List<Integer> identificadores) {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public List<Egresso> buscarDadosEgressoPeloCurso(List<Integer> identificadores) {
-        return criarListaEgressoMock();
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoPeloCurso(List<Integer> identificadores) {
+		return criarListaEgressoMock();
+	}
 
-    @Override
-    public List<Egresso> buscarDadosEgressoPelaUnidadeAcademica(List<Integer> identificadores) {
-        return criarListaEgressoMock();
-    }
+	@Override
+	public List<Egresso> buscarDadosEgressoPelaUnidadeAcademica(List<Integer> identificadores) {
+		return criarListaEgressoMock();
+	}
 
-    public List<Egresso> criarListaEgressoMock(){
-        List<Egresso> egressos = new ArrayList<Egresso>();
+	public List<Egresso> criarListaEgressoMock(){
+		List<Egresso> egressos = new ArrayList<Egresso>();
 
-        for (int i = 0; i < 10; i++) {
-            Egresso egresso = new Egresso("Everton Jose",
-                "Maria",
-                new Date(),
-                Sexo.MASCULINO,
-                "emailAlternativo@gmail.com",
-                new BitSet(),
-                new BitSet(),
-                VisibilidadeDados.PUBLICO,
-                new ArrayList<HistoricoUFG>(),
-                new LocalizacaoGeografica());
-            egressos.add(egresso);
-        }
+		for (int i = 0; i < 10; i++) {
+			Egresso egresso = new Egresso("Everton Jose",
+										  "Maria",
+										  new Date(),
+										  Sexo.MASCULINO,
+										  "emailAlternativo@gmail.com",
+										  new BitSet(),
+										  new BitSet(),
+										  VisibilidadeDados.PUBLICO,
+										  new ArrayList<HistoricoUFG>(),
+                                                                                  new LocalizacaoGeografica());
+			egressos.add(egresso);
+		}
 
-        return egressos;
-    }
+		return egressos;
+	}
 
     @Override
     public List<Egresso> consultarEgressoPorConsultaPreDefinida(String string) {
