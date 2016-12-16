07232d9e1b2343d418714b10aa09095d41527046
LucasFerreiraRodrigue <lucas_f_rodrigues@hotmail.com>
2016-12-16 15:11:24 -0200

    Refatoração do metodo salvar em AprovaçãoDivulgaEventoDao

--- src/main/java/br/ufg/inf/sempreufg/dao/AprovacaoDivulgacaoEventoDAO.java
+++ src/main/java/br/ufg/inf/sempreufg/dao/AprovacaoDivulgacaoEventoDAO.java
@@ -1,43 +1,56 @@
 package br.ufg.inf.sempreufg.dao;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import br.ufg.inf.sempreufg.db.ConexaoBanco;
 import br.ufg.inf.sempreufg.interfaces.AprovacaoDivulgacaoEventoDAOInterface;
 import br.ufg.inf.sempreufg.modelo.AprovacaoDivulgacaoEvento;
 
-public class AprovacaoDivulgacaoEventoDAO implements AprovacaoDivulgacaoEventoDAOInterface{
+public class AprovacaoDivulgacaoEventoDAO implements AprovacaoDivulgacaoEventoDAOInterface {
 
-    private AprovacaoDivulgacaoEvento aprovacaoDivulgacaoEvento;
+	private AprovacaoDivulgacaoEvento aprovacaoDivulgacaoEvento;
 
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("sempreufg");
-    
-    @Override
-    public void salvar(AprovacaoDivulgacaoEvento aprovacaoDivulgacaoEvento) {
-    	EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(aprovacaoDivulgacaoEvento);
-		em.getTransaction().commit();
-    }
+	@Override
+	public void salvar(AprovacaoDivulgacaoEvento aprovacaoDivulgacaoEvento) {
 
-    @Override
-    public void alterar(AprovacaoDivulgacaoEvento aprovacaoDivulgacaoEvento) {
+		String sql = "INSERT INTO aprovacao_de_divulgacao" + " (" + "divulgacao_aprovada,"
+				+ " parecer_sobre_divulgacao, " + "data_aprovacao_ou_rejeicao, " + "evento, " + "usuario_email, "
+				+ "usuario_cpf)" + " values" + "(?,?,?,?,?,?)";
 
-    }
+		try {
+			PreparedStatement ps = ConexaoBanco.getConnection().prepareStatement(sql);
+			ps.setBoolean(1, aprovacaoDivulgacaoEvento.isDivulgacaoAprovada());
+			ps.setString(2, aprovacaoDivulgacaoEvento.getParecerSobreDivulgacao());
+			ps.setDate(3, (Date) aprovacaoDivulgacaoEvento.getDataDoParecer());
+			ps.setString(4, aprovacaoDivulgacaoEvento.getEvento().getAssunto());
+			ps.setString(5, aprovacaoDivulgacaoEvento.getResponsavelPorAprovar().getMail());
+			ps.setLong(6, aprovacaoDivulgacaoEvento.getResponsavelPorAprovar().getCpf());
 
-    @Override
-    public void deletar(int id_AprovacaoDivulgacaoEvento) {
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    }
+	@Override
+	public void alterar(AprovacaoDivulgacaoEvento aprovacaoDivulgacaoEvento) {
 
-    @Override
-    public AprovacaoDivulgacaoEvento getById(int id_Areid_AprovacaoDivulgacaoEvento) {
-        return null;
-    }
+	}
 
-    @Override
-    public boolean obtemStatusAprovacaoEvento() {
-        return aprovacaoDivulgacaoEvento.isDivulgacaoAprovada();
-    }
+	@Override
+	public void deletar(int id_AprovacaoDivulgacaoEvento) {
+
+	}
+
+	@Override
+	public AprovacaoDivulgacaoEvento getById(int id_Areid_AprovacaoDivulgacaoEvento) {
+		return null;
+	}
+
+	@Override
+	public boolean obtemStatusAprovacaoEvento() {
+		return aprovacaoDivulgacaoEvento.isDivulgacaoAprovada();
+	}
 }
